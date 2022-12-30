5fc9f64d20bfc410baffc3b74853afee6c45de61
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-12-30 11:36:22 -0300

    refactor: moving Content, Total, Parth and Header to course directory

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,60 +1,33 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Content = (props) => {
-  const listParts = props.parts.map((part, index) => <Part key={index} part={part} />)
-  return (
-    <div>
-      {listParts}
-    </div>
-  )
-}
-
-const Part = (props) => {
-  const name = props.part.name
-  const exercise = props.part.exercise
-
-  return <p>{name} {exercise}</p>
-}
-
-const Total = (props) => {
-  const parts = props.total
-  let sum = 0
-
-  for (let index = 0; index < parts.length; index++) {
-    sum += parts[index].exercise
-  }
-
-  return <p>Number of exercises {sum}</p>
-}
+import Content from "./course/Content";
+import Header from "./course/Header";
+import Total from "./course/Total";
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercise: 10
+        name: "Fundamentals of React",
+        amount_exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercise: 7
+        name: "Using props to pass data",
+        amount_exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercise: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        amount_exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course.name} />
+      <Header name={course.name} />
       <Content parts={course.parts} />
       <Total total={course.parts} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
