3a916b51dfb35d10fbce3cf48b9c928030359616
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-12 16:39:39 -0300

    feat: add feature to random anecdotes, vote if someone liked it and display the total votes of any anedocte

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Button from "./Button";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const length = anecdotes.length;
+  const [points, setPoints] = useState(new Array(length).fill(0));
+
+  function getRandomIntInclusive(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  function handleVotes(positionAnecdote) {
+    const copy = [...points];
+    copy[positionAnecdote]++;
+    setPoints(copy);
+  }
+
+  return (
+    <section>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <div>
+        <Button handleClick={() => handleVotes(selected)} text="vote" />
+        <Button
+          handleClick={() => setSelected(getRandomIntInclusive(length - 1))}
+          text="next anecdote"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default App;
