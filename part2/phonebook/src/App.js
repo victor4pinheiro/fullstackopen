88ede7308a54821640b615a7e7e626c81acf51b9
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-01-18 13:40:44 -0300

    feat: use useEffect with axios to fetch data from database and change phone to number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import PersonFilter from "./components/PersonFilter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "040-123456", id: 1 },
-    { name: "Ada Lovelace", phoneNumber: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", phoneNumber: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", phoneNumber: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
   const [newNameSearch, setNewNameSearch] = useState("");
   const [newResults, setNewResults] = useState([]);
+
+  const hookPersons = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -24,7 +26,7 @@ const App = () => {
       const newPerson = {
         id: persons.length + 1,
         name: newName,
-        phoneNumber: newPhoneNumber,
+        number: newPhoneNumber,
       };
 
       setPersons(persons.concat(newPerson));
@@ -49,6 +51,7 @@ const App = () => {
     setNewNameSearch(value);
   };
 
+  useEffect(hookPersons, []);
   return (
     <div>
       <h2>Phonebook</h2>
